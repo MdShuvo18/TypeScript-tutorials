cae478197e988b6cd08dd146067b3290fc3827e8
RakibHassanSoft <rakibreings32@gmail.com>
2024-11-12 01:37:57 +0600

    fucntion2.ts updated

--- Day4/TypeAliases.ts
+++ Day4/TypeAliases.ts
@@ -4,8 +4,16 @@
      let val : strOrNum = "EMon";
      let val2 : strOrNum = 2;
 
+     //working with type aliases
      type Status =  "pending" | "done"
      const data  : Status = "pending"
+
+    // Working with union type
+    let Status1 : "pending" | "done" | "failed"=  "done" 
+     data1  : Status1 = "done"
+
+
+
 
      type AddFun = (a: number , b : number)=> number;
      const add : AddFun = (a,b)=> a + b;
